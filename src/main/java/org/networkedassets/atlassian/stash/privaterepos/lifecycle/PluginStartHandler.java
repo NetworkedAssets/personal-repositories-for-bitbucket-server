7f1bfb46f299d2b97580ed280e3270897cf384b7
Konrad Hołowiński <kholowinski@networkedassets.org>
2014-07-12 17:53:21 +0200

    Switched back to PluginEnabledEvent - it turns out that we can't use @PostConstruct - AO is not initialized when it's fired

--- src/main/java/org/networkedassets/atlassian/stash/privaterepos/lifecycle/PluginStartHandler.java
+++ src/main/java/org/networkedassets/atlassian/stash/privaterepos/lifecycle/PluginStartHandler.java
@@ -1,18 +1,25 @@
 package org.networkedassets.atlassian.stash.privaterepos.lifecycle;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.networkedassets.atlassian.stash.privaterepos.repositories.PersonalRepositoriesPreScanner;
-import org.networkedassets.atlassian.stash.privaterepos.repositories.PersonalRepositoriesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.atlassian.event.api.EventListener;
+import com.atlassian.event.api.EventPublisher;
 import com.atlassian.plugin.event.events.PluginEnabledEvent;
 
 @Component
 public class PluginStartHandler {
+
+	private static final String PLUGIN_KEY = "org.networkedassets.atlassian.stash.privaterepos";
+
+	@Autowired
+	private EventPublisher eventPublisher;
 
 	@Autowired
 	private PersonalRepositoriesPreScanner personalRepositoriesPreScanner;
@@ -21,8 +28,20 @@ public class PluginStartHandler {
 			.getLogger(PluginStartHandler.class);
 
 	@PostConstruct
-	public void handlePluginEnabledEvent(PluginEnabledEvent event) {
-		personalRepositoriesPreScanner.scanPersonalRepositories();
+	public void registerEvents() {
+		eventPublisher.register(this);
+	}
+
+	@PreDestroy
+	public void unregisterEvents() {
+		eventPublisher.unregister(this);
+	}
+
+	@EventListener
+	public void onPluginEnabledEvent(PluginEnabledEvent event) {
+		if (event.getPlugin().getKey().equals(PLUGIN_KEY)) {
+			personalRepositoriesPreScanner.scanPersonalRepositories();
+		}
 	}
 
 }
--- src/main/java/org/networkedassets/atlassian/stash/privaterepos/repositories/PersonalRepositoriesPreScanner.java
+++ src/main/java/org/networkedassets/atlassian/stash/privaterepos/repositories/PersonalRepositoriesPreScanner.java
@@ -2,6 +2,8 @@ package org.networkedassets.atlassian.stash.privaterepos.repositories;
 
 import org.networkedassets.atlassian.stash.privaterepos.util.AllPagesIterator;
 import org.networkedassets.atlassian.stash.privaterepos.util.PageProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,8 @@ import com.atlassian.stash.util.PageRequest;
 
 @Component
 public class PersonalRepositoriesPreScanner {
+
+	Logger log = LoggerFactory.getLogger(PersonalRepositoriesPreScanner.class);
 
 	@Autowired
 	PersonalRepositoriesService personalRepositoriesService;
@@ -34,11 +38,13 @@ public class PersonalRepositoriesPreScanner {
 		return new PageProcessor<StashUser>() {
 
 			public Page<? extends StashUser> fetchPage(PageRequest pageRequest) {
+				log.warn("Fetching page {}", pageRequest.getStart());
 				return userService.findUsers(pageRequest);
 			}
 
 			public void process(Page<? extends StashUser> page) {
 				for (StashUser user : page.getValues()) {
+					log.warn("Processing user {}", user.getName());
 					scanUserRepositories(user);
 				}
 			}
@@ -58,12 +64,14 @@ public class PersonalRepositoriesPreScanner {
 
 			@Override
 			public void process(Page<? extends Repository> page) {
+				log.warn("Processing repositories {}", page.getValues());
 				personalRepositoriesService.rememberUserPersonalRepositories(
 						user, page.getValues());
 			}
 
 			@Override
 			public Page<? extends Repository> fetchPage(PageRequest pageRequest) {
+				log.warn("Fetching repositories for {}", ("~" + user.getSlug()));
 				return repositoryService.findByProjectKey("~" + user.getSlug(),
 						pageRequest);
 			}
