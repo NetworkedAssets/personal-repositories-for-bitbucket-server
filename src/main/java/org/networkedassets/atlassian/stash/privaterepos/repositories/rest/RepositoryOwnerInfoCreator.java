87c2efc6408068098354514541f95aeaf7c14bee
Konrad Hołowiński <kholowinski@networkedassets.org>
2014-07-15 20:36:53 +0200

    Optimized version for list creation - using one userService query

--- src/main/java/org/networkedassets/atlassian/stash/privaterepos/repositories/rest/RepositoryOwnerInfoCreator.java
+++ src/main/java/org/networkedassets/atlassian/stash/privaterepos/repositories/rest/RepositoryOwnerInfoCreator.java
@@ -1,7 +1,10 @@
 package org.networkedassets.atlassian.stash.privaterepos.repositories.rest;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.networkedassets.atlassian.stash.privaterepos.repositories.Owner;
 import org.networkedassets.atlassian.stash.privaterepos.user.rest.UsersInfoCreator;
@@ -22,16 +25,24 @@ class RepositoryOwnerInfoCreator {
 
 	public List<RepositoryOwnerInfo> create(List<Owner> owners) {
 		List<RepositoryOwnerInfo> ownersInfo = new ArrayList<RepositoryOwnerInfo>();
+
+		Set<? extends StashUser> stashUsers = getStashUsersFromOwners(owners);
+		Iterator<? extends StashUser> stashUsersIterator = stashUsers
+				.iterator();
+
 		for (Owner owner : owners) {
-			ownersInfo.add(create(owner));
+			ownersInfo.add(create(owner, stashUsersIterator.next()));
 		}
 		return ownersInfo;
 	}
 
 	public RepositoryOwnerInfo create(Owner owner) {
+		return create(owner, getStashUserFromOwner(owner));
+	}
+
+	public RepositoryOwnerInfo create(Owner owner, StashUser user) {
 		RepositoryOwnerInfo ownerInfo = new RepositoryOwnerInfo();
-		usersInfoCreator.fillUserInfoWithStashUserData(ownerInfo,
-				getStashUserFromOwner(owner));
+		usersInfoCreator.fillUserInfoWithStashUserData(ownerInfo, user);
 		fillOwnerInfoWithOwnerData(ownerInfo, owner);
 		return ownerInfo;
 	}
@@ -43,6 +54,14 @@ class RepositoryOwnerInfoCreator {
 
 	private StashUser getStashUserFromOwner(Owner owner) {
 		return userService.getUserById(owner.getUserId());
+	}
+
+	private Set<? extends StashUser> getStashUsersFromOwners(List<Owner> owners) {
+		Set<Integer> ids = new HashSet<Integer>();
+		for (Owner owner : owners) {
+			ids.add(owner.getUserId());
+		}
+		return userService.getUsersById(ids);
 	}
 
 }
