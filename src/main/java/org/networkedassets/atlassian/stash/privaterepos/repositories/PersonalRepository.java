ceb4b9cb5499dbd8ed9132d201f9070f4bbf0e67
Konrad Hołowiński <kholowinski@networkedassets.org>
2014-08-19 19:47:10 +0200

    Imports organized

--- src/main/java/org/networkedassets/atlassian/stash/privaterepos/repositories/PersonalRepository.java
+++ src/main/java/org/networkedassets/atlassian/stash/privaterepos/repositories/PersonalRepository.java
@@ -1,7 +1,6 @@
 package org.networkedassets.atlassian.stash.privaterepos.repositories;
 
 import net.java.ao.Entity;
-import net.java.ao.schema.Unique;
 
 public interface PersonalRepository extends Entity {
 
