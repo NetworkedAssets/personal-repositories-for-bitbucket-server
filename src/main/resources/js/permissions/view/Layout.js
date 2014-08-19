207c91e94633366da1b3d540c6d6ed810a4cf3b7
Konrad Hołowiński <kholowinski@networkedassets.org>
2014-08-19 02:23:50 +0200

    Buttons pressed state changing now

--- src/main/resources/js/permissions/view/Layout.js
+++ src/main/resources/js/permissions/view/Layout.js
@@ -1,6 +1,24 @@
-define('PermissionsLayout', [ 'backbone', 'underscore' ], function(Backbone, _) {
+define('PermissionsLayout', [ 'backbone', 'underscore', 'jquery' ], function(Backbone, _, $) {
 	return Backbone.View.extend({
 		template : org.networkedassets.personalRepos.permissions.layout,
+		
+		pressedButtonAttr : 'aria-pressed',
+		
+		events : {
+			'click .repository-permissions-mode .aui-button': 'onModeButtonClick'
+		},
+		
+		onModeButtonClick : function(e) {
+			var $el = $(e.currentTarget);
+			if (!this.isPressed($el)) {
+				this.$('.aui-button[aria-pressed="true"]').attr('aria-pressed', 'false');
+				$el.attr('aria-pressed', 'true');
+			}
+		},
+		
+		isPressed : function($el) {
+			return ($el.attr('aria-pressed') == 'true');
+		},
 		
 		render : function() {
 			this.el.innerHTML = this.template();
