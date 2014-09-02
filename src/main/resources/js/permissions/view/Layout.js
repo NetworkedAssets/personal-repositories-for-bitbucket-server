faa3a0fcdd7325cd1692d4e3e5820504cf8779f9
Konrad Hołowiński <kholowinski@networkedassets.org>
2014-09-02 23:50:31 +0200

    Dialog on mode change

--- src/main/resources/js/permissions/view/Layout.js
+++ src/main/resources/js/permissions/view/Layout.js
@@ -14,10 +14,36 @@ define('PermissionsLayout', [ 'backbone', 'underscore', 'jquery' ], function(Bac
 		onModeButtonClick : function(e) {
 			var $el = $(e.currentTarget);
 			if (!this.isPressed($el)) {
-				this.$('.aui-button[aria-pressed="true"]').attr('aria-pressed', 'false');
-				$el.attr('aria-pressed', 'true');
-				this.trigger('mode-changed', $el.hasClass('allow') ? 'allow' : 'deny')
+				this.showConfirmation({
+					ok : _.bind(function() {
+						this.switchMode($el);
+					}, this)
+				});
 			}
+		},
+		
+		showConfirmation : function(opts) {
+			var dialog = new AJS.Dialog({
+			    width: 500, 
+			    height: 250, 
+			    closeOnOutsideClick: true
+			});
+			dialog.addHeader(AJS.I18n.getText('org.networkedassets.atlassian.stash.personalstash.permissions.mode.change.confirmation.header'));
+			dialog.addPanel("Panel", AJS.I18n.getText('org.networkedassets.atlassian.stash.personalstash.permissions.mode.change.confirmation.text'), "panel-body");
+			dialog.addButton(AJS.I18n.getText("org.networkedassets.atlassian.stash.personalstash.permissions.mode.change.confirmation.button.cancel"), function (dialog) {
+			    dialog.hide();
+			});
+			dialog.addButton(AJS.I18n.getText("org.networkedassets.atlassian.stash.personalstash.permissions.mode.change.confirmation.button.ok"), _.bind(function (dialog) {
+				dialog.hide();
+				opts.ok();
+			}, this));
+			dialog.show();
+		},
+		
+		switchMode : function($el) {
+			this.$('.aui-button[aria-pressed="true"]').attr('aria-pressed', 'false');
+			$el.attr('aria-pressed', 'true');
+			this.trigger('mode-changed', $el.hasClass('allow') ? 'allow' : 'deny')
 		},
 		
 		isPressed : function($el) {
